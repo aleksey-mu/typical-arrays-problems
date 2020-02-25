f8891b11e8461577444b304a4a7bda8dd97d53fb
aleksey_mu <pacuklex@mail.ru>
2020-02-25 11:58:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  let min = array[0];
+  for (num of array) {
+    if (num < min) {
+      min = num;
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  let max = array[0];
+  for (num of array) {
+    if (num > max) {
+      max = num;
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (num of array) {
+    sum += num;
+  }
+  return sum / array.length;
 }
